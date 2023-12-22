1421adbbde1383cbd8c75dff9166fe6a5aaacf9c
mekdes975 <mekmeskassa@gmail.com>
2023-12-22 09:38:44 +0300

    extracting data

--- airflow/dags/src/extract_data_source.py
+++ airflow/dags/src/extract_data_source.py
@@ -1,32 +1,28 @@
 
 import pandas as pd
-
-def get_data(**kwargs):
+class get_data:
+    '''
+    this class divide the whole csv data into two and extract them.
+    '''
     # Define the path to your CSV file
     csv_path = '/home/hp/Downloads/20181024_d1_0830_0900(2).csv'
+    def __init__(self) -> None:
+        pass
 
-    # Read the CSV file using pandas
-    try:
+    def get_data1(**kwargs):
+        
         # Read the CSV file into a pandas DataFrame
         df= pd.read_csv('/home/hp/Downloads/20181024_d1_0830_0900(2).csv',index_col=False,delimiter = '; ')
-
+        df1 = df.iloc[:, :4]
         
-        # You can perform additional data preprocessing steps here if needed
-         # Split the single column into multiple columns based on semicolon separator
+       
+        return df1
+    def get_data1(**kwargs):
         
-        
-        # Optional: Assign meaningful column names to the new columns
-        #df.columns = ['Attribute_1', 'Attribute_2', 'Attribute_3', 'Attribute_4', 'Attribute_5',
-         #             'Attribute_6', 'Attribute_7', 'Attribute_8', 'Attribute_9', 'Attribute_10']
-        
-        # Perform any additional preprocessing steps here
-        
-        # Return the preprocessed DataFrame
-        return df
-        
-        
-        
-    except Exception as e:
-        # Handle exceptions if any
-        print(f"An error occurred while reading the CSV file: {e}")
-        return None
+        # Read the CSV file into a pandas DataFrame
+        df= pd.read_csv('/home/hp/Downloads/20181024_d1_0830_0900(2).csv',index_col=False,delimiter = '; ')
+        df2 = df.iloc[:, 5:]
+        return df2
+            
+            
+       
