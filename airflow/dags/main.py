734a40b5a1e60c96cbfbcb05d2eaab410b5c494f
mekdes975 <mekmeskassa@gmail.com>
2023-12-25 02:34:38 +0300

    extracting task is added to the dag

--- airflow/dags/main.py
+++ airflow/dags/main.py
@@ -1,79 +1,10 @@
 
-'''from datetime import datetime, timedelta
-from airflow import DAG
-from airflow.operators.python import PythonOperator
-import sys 
-import os 
-
-
-
-
-
-
-import requests
-import json
-import os
-
-#with DAG("my_dag", start_date=(2023,12,20),schedule_interval="@daily",catchup=False)as dag:
-
-
-# Define the default dag arguments.
-default_args = {
-    'owner': 'admin',
-    'depends_on_past': False,
-    'email': ['mekmeskassa@gmail.com'],
-    'start_date': datetime(2023,12,21),
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 5,
-    'retry_delay': timedelta(minutes=2)
-}
-
-
-# Define the dag, the start date and how frequently it runs.
-# I chose the dag to run everday by .
-dag = DAG(
-    dag_id='new_dag',
-    default_args=default_args,
-    catchup=False,
-    schedule_interval= "@daily" ) 
-
-
-# First task is to query get the weather from openweathermap.org.
-task1 = PythonOperator(
-    task_id='get_data',
-    provide_context=True,
-    python_callable=get_data,
-    dag=dag)
-
-
-# Second task is to transform the data
-task2 = PythonOperator(
-    task_id='load_data',
-    provide_context=True,
-    python_callable=load_data,
-    dag=dag)
-
-# Third task is to transform data into the database.
-task3 = PythonOperator(
-    task_id='transform_data',
-    provide_context=True,
-    python_callable=transform_data,
-    dag=dag)
-
-# Set task1 "upstream" of task2
-# task1 must be completed before task2 can be started
-task1 >> task2 >> task3
-
-'''
-
-
-
-
 from datetime import datetime, timedelta
 from airflow import DAG
 #from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python import PythonOperator
+
+from src.extract_data_source import CSVDataReader
 
 # Define default_args dictionary to specify the default parameters of the DAG
 default_args = {
@@ -96,7 +27,12 @@ dag = DAG(
 
 
 def task_a_function():
-    print("Extracting Data")
+    csv_reader = CSVDataReader('/home/hp/Downloads/20181024_d1_0830_0900(2).csv')
+    csv_reader.read_data()
+    df_track = csv_reader.df_track
+    df_Trajectory = csv_reader.df_Trajectory
+    return df_track, df_Trajectory
+    
 
 task_a_task = PythonOperator(
     task_id='Extract',
