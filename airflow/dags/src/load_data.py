f4f5b12f3765a6406dedf3f9e8be8c75aaff5ced
mekdes975 <mekmeskassa@gmail.com>
2023-12-25 04:43:23 +0300

    load data source file

--- airflow/dags/src/load_data.py
+++ airflow/dags/src/load_data.py
@@ -10,25 +10,75 @@ if parent_directory not in sys.path:
 
 
 from sqlalchemy import create_engine
-from src.extract_data_source import get_data
-
+from src.extract_data_source import CSVDataReader
 
 
 import psycopg2
+from psycopg2 import sql
 from sqlalchemy import create_engine
-import pandas as pd
 
-# Load data from CSV files into DataFrames
-data1 = get_data.get_data1  #  get_data1() returns DataFrame for track table
-data2 = get_data.get_data2  #  get_data2() returns DataFrame for trajectory table
+csv_reader = CSVDataReader('/home/hp/Downloads/20181024_d1_0830_0900(2).csv')
+csv_reader.read_data()
+df_track = csv_reader.df_track
+df_Trajectory = csv_reader.df_Trajectory
 
-# Merge the DataFrames into a single DataFrame
-merged_data = pd.concat([data1, data2], axis=1)  # Combine columns
+# Define your PostgreSQL connection parameters
+connection_params = {
+    "dbname": 'trafficflow',
+    "user": "postres",
+    "password": "1234",
+    "host": "localhost",
+    "port": "5438"
+}
 
-# Create the connection string
-engine = create_engine('postgresql://postgres:1234@localhost:5432/trafficflow')
+# Establish a connection to PostgreSQL
+conn = psycopg2.connect(**connection_params)
+cursor = conn.cursor()
 
-# Load data into PostgreSQL table
-merged_data.to_sql('trafficflow', engine, if_exists='replace', index=False)
+# Create a schema if it doesn't exist
+schema_name = 'trafficdata'
+cursor.execute(f"CREATE SCHEMA IF NOT EXISTS {schema_name}")
+conn.commit()
 
-   
+
+# Define table creation query for the track table
+create_track_table_query = """
+    CREATE TABLE IF NOT EXISTS trafficdata.track (
+        track_id INT,
+        type VARCHAR(50),
+        avg_speed FLOAT,
+        traveled_d FLOAT
+    )
+"""
+
+
+# Define table creation query for the trajectory table
+create_trajectory_table_query = """
+    CREATE TABLE IF NOT EXISTS trafficdata.trajectory (
+        track_id INT,
+        lat FLOAT,
+        lon FLOAT,
+        speed FLOAT,
+        lon_acc FLOAT,
+        lat_acc FLOAT,
+        time FLOAT
+    )
+"""
+
+
+# Execute table creation queries
+cursor.execute(create_track_table_query)
+cursor.execute(create_trajectory_table_query)
+conn.commit()
+
+# Create SQLAlchemy engine
+engine = create_engine('postgresql://postgres:1234@localhost:5438/trafficflow')
+
+# Load df_track and df_Trajectory into respective tables
+df_track.to_sql('track', engine, schema=schema_name, if_exists='replace', index=False)
+df_Trajectory.to_sql('trajectory', engine, schema=schema_name, if_exists='replace', index=False)
+
+# Close the cursor and connection
+cursor.close()
+conn.close()
+
