586b46500b3047bbf26c18c93b175a419585609e
mekdes975 <mekmeskassa@gmail.com>
2023-12-25 05:30:51 +0300

    added

--- airflow/dags/main.py
+++ airflow/dags/main.py
@@ -14,7 +14,7 @@ default_args = {
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    
 }
 
 # Instantiate a DAG with the defined default_args
